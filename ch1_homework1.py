2b01d2b8509afdc43919f09e5c05c0662ce034be
Zhongqiang Shen <shenzhongqiang@msn.com>
2019-11-06 16:42:38 +0800

    chapter 1 homework 1

---
+++ ch1_homework1.py
@@ -0,0 +1,18 @@
+contacts = [
+    {
+        'name': 'Jack',
+        'tel': '131111111',
+        'district': 'Shanghai',
+    },
+    {
+        'name': 'John',
+        'tel': '132222222',
+        'district': 'Beijing',
+    },
+    {
+        'name': 'Jane',
+        'tel': '135555555',
+        'district': 'Shanghai',
+    }
+]
+print(contacts)
